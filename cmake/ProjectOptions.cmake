da192f62618b8ec25a8cf7c953636353759e2271
Denys Asauliak <denoming@gmail.com>
2022-06-06 20:28:43 +0300

    fix option names

--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -1,8 +1,8 @@
 include(FeatureSummary)
 
-option(TEMPLATE01_ENABLE_TESTS "Enable testing" ON)
+option(JARVIS_ENABLE_TESTS "Enable testing" ON)
 add_feature_info(
-    ENABLE_TESTS TEMPLATE01_ENABLE_TESTS "Build project with tests"
+    ENABLE_TESTS JARVIS_ENABLE_TESTS "Build project with tests"
 )
 
 feature_summary(WHAT ALL)
