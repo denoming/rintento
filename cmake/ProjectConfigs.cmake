921a425f8648757d6bd25d403f9f514f18fdbbba
Denys Asauliak <denoming@gmail.com>
2022-06-12 19:27:42 +0300

    specify C++ standard

--- cmake/ProjectConfigs.cmake
+++ cmake/ProjectConfigs.cmake
@@ -4,6 +4,10 @@ if (EXISTS $ENV{HOME}/.local)
     list(APPEND CMAKE_PREFIX_PATH $ENV{HOME}/.local)
 endif()
 
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
 include(BuildType)
 include(BuildLocation)
 include(BuildOptions)
