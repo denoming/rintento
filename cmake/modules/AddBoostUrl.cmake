7494b5fc149399f97b9daa191c074c6f70a7b948
Denys Asauliak <denoming@gmail.com>
2023-02-18 20:46:46 +0200

    update boost url addr

--- cmake/modules/AddBoostUrl.cmake
+++ cmake/modules/AddBoostUrl.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 FetchContent_Declare(boost-url
-    GIT_REPOSITORY https://github.com/CPPAlliance/url.git
+    GIT_REPOSITORY https://github.com/boostorg/url.git
     GIT_TAG 954eb5b7e6af8228fa3b8a644c6a2e51b29980e7
 )
 
