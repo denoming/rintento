8ff847072647c63d8d636e96b3e784c9c6258227
Denys Asauliak <denoming@gmail.com>
2023-08-13 23:35:40 +0300

    add missing logger init

--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -5,4 +5,9 @@ add_feature_info(
     ENABLE_TESTS ENABLE_TESTS "Build project with tests"
 )
 
+option(ENABLE_DLT "Enable DLT logging" OFF)
+add_feature_info(
+    ENABLE_DLT ENABLE_DLT "Build project with DLT logging"
+)
+
 feature_summary(WHAT ALL)
--- src/ServiceApplication.cpp
+++ src/ServiceApplication.cpp
@@ -2,8 +2,10 @@
 
 #include "common/Config.hpp"
 #include "intent/IntentSubsystem.hpp"
+#include "rintento/Options.hpp"
 
 #include <jarvisto/Logger.hpp>
+#include <jarvisto/LoggerInitializer.hpp>
 
 namespace jar {
 
@@ -16,10 +18,15 @@ ServiceApplication::name() const
 void
 ServiceApplication::initialize(Application& application)
 {
-    _config = std::make_shared<Config>();
+#ifdef ENABLE_DLT
+    LoggerInitializer::instance().initialize("RINT", name(), "MAIN", "Main Context");
+#else
+    LoggerInitializer::instance().initialize();
+#endif
 
+    _config = std::make_shared<Config>();
     if (!_config->load()) {
-        LOGE("Failed to load config file");
+        LOGE("Unable to load config file");
     }
 
     addSubsystem(std::make_unique<IntentSubsystem>(_config));
