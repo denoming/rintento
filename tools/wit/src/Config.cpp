1e8db0afbfa7603d9b6df25599beaa3bf127713c
Denys Asauliak <denoming@gmail.com>
2023-09-30 22:49:14 +0300

    fix names and add clearing

--- tools/wit/src/Config.cpp
+++ tools/wit/src/Config.cpp
@@ -27,28 +27,35 @@ Config::doParse(const boost::property_tree::ptree& root)
 {
     if (auto hostOpt = root.get_optional<std::string>("wit.remote.host"); hostOpt) {
         if (hostOpt->empty()) {
-            LOGW("Invalid host option value");
+            LOGW("Invalid 'host' option value");
+            _remoteHost.clear();
         } else {
             _remoteHost = std::move(*hostOpt);
         }
+    } else {
+        LOGW("Mandatory 'host' option value is absent");
     }
 
     if (auto portOpt = root.get_optional<std::string>("wit.remote.port"); portOpt) {
         if (portOpt->empty()) {
-            LOGW("Invalid port option value: {}", *portOpt);
+            LOGW("Invalid 'port' option value: {}", *portOpt);
+            _remotePort.clear();
         } else {
             _remotePort = std::move(*portOpt);
         }
+    } else {
+        LOGW("Mandatory 'port' option value is absent");
     }
 
     if (auto authOpt = root.get_optional<std::string>("wit.remote.auth"); authOpt) {
         if (authOpt->empty()) {
-            LOGW("Invalid auth option value");
+            LOGW("Invalid 'auth' option value");
+            _remoteAuth.clear();
         } else {
             _remoteAuth = std::move(*authOpt);
         }
     } else {
-        LOGW("Mandatory recognize auth option value is absent");
+        LOGW("Mandatory 'auth' option value is absent");
     }
 }
 
