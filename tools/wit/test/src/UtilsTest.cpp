4b87bab7a317429cb3a36360cd1194dee7013812
Denys Asauliak <denoming@gmail.com>
2023-10-01 11:15:45 +0300

    split utils testing

---
+++ tools/wit/test/src/UtilsTest.cpp
@@ -0,0 +1,36 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "wit/Utils.hpp"
+
+using namespace jar;
+using namespace testing;
+
+#include <regex>
+
+TEST(UtilsTest, MessageTarget)
+{
+    static const std::string_view in{"turn on the light"};
+    EXPECT_THAT(wit::messageTarget(in), Eq("/message?q=turn+on+the+light"));
+}
+
+TEST(UtilsTest, MessageTargetWithDate)
+{
+    static const std::string_view in{"turn on the light"};
+    const auto out = wit::messageTargetWithDate(in);
+    static const std::regex re{R"(\/message\?v=\d{8}\&q=turn\+on\+the\+light)"};
+    EXPECT_THAT(std::regex_match(out, re), IsTrue());
+}
+
+TEST(UtilsTest, SpeechTarget)
+{
+    const auto out = wit::speechTarget();
+    EXPECT_THAT(out, Eq("/speech"));
+}
+
+TEST(UtilsTest, SpeechTargetWithDate)
+{
+    const auto out = wit::speechTargetWithDate();
+    static const std::regex re{R"(\/speech\?v=\d{8})"};
+    EXPECT_THAT(std::regex_match(out, re), IsTrue());
+}
