4936ea81dc04580a7ed259fcf413b15426bc1012
Denys Asauliak <denoming@gmail.com>
2025-08-29 22:20:36 +0300

    chore: use apache license
    [no ci]

--- scripts/install-arm64.sh
+++ scripts/install-arm64.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Copyright 2025 Denys Asauliak
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 JARVISTO_URL1=https://github.com/denoming/jarvisto/releases/download/v0.3.11/libjarvisto_0.3.11_arm64.deb
 JARVISTO_URL2=https://github.com/denoming/jarvisto/releases/download/v0.3.11/libjarvisto-dev_0.3.11_arm64.deb
 
